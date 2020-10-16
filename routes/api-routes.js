9b4bcabe351a05e6cc27ce75d387dc1a004471ad
guillenjoshua <guillenjoshua@gmail.com>
2020-10-16 09:23:31 -0500

    Adding html files and html routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,29 @@
-module.exports = function(app) {}
+module.exports = function(app) {
+
+
+    //Search all breweries
+    app.get("/api/breweries", function(req, res) {
+       
+        db.breweries.findAll({
+          include: [db.breweries]
+        }).then(function(dbbreweries) {
+          res.json(dbbreweries);
+        });
+      });
+
+        //Search single brewery
+      app.get("/api/breweries/:id", function(req, res) {
+      
+        db.breweries.findOne({
+          where: {
+            id: req.params.id
+          },
+          include: [db.breweries]
+        }).then(function(dbbreweries) {
+          res.json(dbbreweries);
+        });
+      });
+
+
+    
+}
